a64680cd5c53d4caa1841cd2645bbae9db1cb836
mrgnurt <mrgnurt1995@gmail.com>
2023-05-24 09:23:08 +0700

    refactor

--- src/main/java/com/example/demoes/infrastruture/TransactionRepository.java
+++ src/main/java/com/example/demoes/infrastruture/TransactionRepository.java
@@ -1,7 +1,12 @@
 package com.example.demoes.infrastruture;
 
 import com.example.demoes.model.Transaction;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 public interface TransactionRepository extends ElasticsearchRepository<Transaction, String> {
+    Page<Transaction> findTransactionByPartnerId(String partnerId, Pageable pageable);
+
+    Transaction findTransactionByIdAndPartnerId(String id, String partnerId);
 }
